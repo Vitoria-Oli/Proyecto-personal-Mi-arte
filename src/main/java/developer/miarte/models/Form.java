121446d88952e07874efbfb3816f4ce505194d15
Vitoria Oliveira <vitoria.oli.san14@gmail.com>
2023-04-22 18:06:27 +0200

    start backand

--- src/main/java/developer/miarte/models/Form.java
+++ src/main/java/developer/miarte/models/Form.java
@@ -1,9 +1,36 @@
 package developer.miarte.models;
 
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
 
-
+@ToString
+@Setter
+@EqualsAndHashCode
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@@Table(name = "posts")
 public class Form {
     
 }
